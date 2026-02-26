396690fc1e46f5860c806bca8f60e007caea856d
Abidau <naufalabidau@gmail.com>
2026-02-26 14:14:55 +0700

    Prak2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PhotoController;
 
-Route::get('/user/{name?}', function ($name='John') {
-    return 'Nama saya '.$name;
-});
+// Route::get('/user/{name?', function ($name = 'John') {
+//     return 'Nama Saya ' . $name;
+// });
+
+Route::resource('photos', PhotoController::class) ->only([
+    'index', 'show'
+]);
+
+Route::resource('photos', PhotoController::class) ->except([
+    'create', 'store', 'update', 'destroy',
+]);
+
+Route::get('/hello', [WelcomeController::class, 'hello']);
